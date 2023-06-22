ca1617a665f5f8fc5700cf90e70321dd91ec4b9a
denverma7 <126649187+denverma7@users.noreply.github.com>
2023-06-22 11:45:44 +0500

    database function changes

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -10,6 +10,7 @@ export class AppServiceService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
+    this.ROOT_URL = 'http://localhost:8080'
     if(environment.production == false){
       this.ROOT_URL = 'test'
     }else{
